b8707c64f4350a3ab7272c2a1fef6213ba494185
Joakim <joakimj15@gmail.com>
2017-08-30 15:24:24 +0200

    added 10 questions

--- code.js
+++ code.js
@@ -2,71 +2,95 @@ var app = angular.module("quiz",[]);
 
 app.controller("quizController", function($scope){
     $scope.questions = [
-      {       image : "images/r2-d2.jpeg",
-              choices : [
-                      "C-3PO",
-                      "QT-KT",
-                      "R5-D404",
-                      "R2-D2"
-                        ],
-              correct : "R2-D2"
+        {   image : "images/r2-d2.jpeg",
+            choices : [
+                "C-3PO",
+                "QT-KT",
+                "R5-D404",
+                "R2-D2"
+            ],
+            correct : "R2-D2"
         },
-        {       image : "images/admiral-ackbar.jpeg",
-                choices : [
-                        "Adiral Motti",
-                        "Admiral Ackbar",
-                        "General Hux",
-                        "Galen Erso"
-                          ],
-                correct : "Admiral Ackbar"
-          }
+        {   image : "images/admiral-ackbar.jpeg",
+            choices : [
+                "Admiral Motti",
+                "Admiral Ackbar",
+                "General Hux",
+                "Galen Erso"
+            ],
+            correct : "Admiral Ackbar"
+        },
+        {   image : "images/biggs-darklighter.jpeg",
+            choices : [
+                "Captain Needa",
+                "Biggs Darklighter",
+                "Anakin Skywalker",
+                "Jar Jar Quiz"
+            ],
+            correct : "Biggs Darklighter"
+        },
+        {   image : "images/dengar.jpeg",
+            choices : [
+                "Silman",
+                "Rancor-Keeper",
+                "Oola",
+                "Dengar"
+            ],
+            correct : "Dengar"
+        },
+        {   image : "images/saw-gerrera.jpeg",
+            choices : [
+                "Bail Organa",
+                "Saw Gerrera",
+                "Mace Windu",
+                "Unkar Plutt"
+            ],
+            correct : "Saw Gerrera"
+        },
+        {   image : "images/mon-mothma.jpeg",
+            choices : [
+                "Obi-Wan Kenobi",
+                "Galen Erso",
+                "Ponda Baba",
+                "Mon Mothma"
+            ],
+            correct : "Mon Mothma"
+        },
+        {   image : "images/captain-phasma.jpeg",
+            choices : [
+                "Count Dooku",
+                "Captain Phasma",
+                "EV-9D9",
+                "Salacious B Crumb"
+            ],
+            correct : "Captain Phasma"
+        },
+        {   image : "images/darth-vader.jpeg",
+            choices : [
+                "Captain Phasma",
+                "Stormtrooper",
+                "Darth Vader",
+                "Max Rebo Band"
+            ],
+            correct : "Darth Vader"
+        },
+        {   image : "images/c2.b5.jpeg",
+            choices : [
+                "C-3PO",
+                "K.2SO",
+                "4-LOM",
+                "C2-B5"
+            ],
+            correct : "C2-B5"
+        },
+        {   image : "images/bib-fortuna.jpeg",
+            choices : [
+                "Bib Fortuna",
+                "Grand Quiz Tarkin",
+                "Nien Numb",
+                "Sebulba"
+            ],
+            correct : "Bib Fortuna"
+        }
         ]
 });
-
-
-
-    // ]
-    // $scope.option = [
-    //     "4-Lom",
-    //     "Adiral Motti",
-    //     "Anakin Skywalker",
-    //     "Bail Organa",
-    //     "BG-81",
-    //     "Bossk",
-    //     "Captain Needa",
-    //     "Ello Asty",
-    //     "EV-9D9",
-    //     "Galen Erso",
-    //     "General Hux",
-    //     "General Tarpas",
-    //     "Greedo",
-    //     "Han Solo",
-    //     "IG-88",
-    //     "Jabba The Hutt",
-    //     "Jar Jar Quiz",
-    //     "K-2SO",
-    //     "Kit Fisto",
-    //     "Kylo Ren",
-    //     "Lando Calrissian",
-    //     "Lobot",
-    //     "Luke Skywalker",
-    //     "Mace Windu",
-    //     "Max Rebo",
-    //     "Nien Numb",
-    //     "Obi-Wan Kenobi",
-    //     "Oola",
-    //     "Paploo",
-    //     "Poe Dameron",
-    //     "Ponda Baba",
-    //     "QT-KT",
-    //     "R5-D4",
-    //     "Rancor-Keeper",
-    //     "Salacious B Crumb",
-    //     "Sebulba",
-    //     "Silman",
-    //     "TC-14",
-    //     "Unkar Plutt",
-    //     "Watto",
-    //     "Wedge Antilles",
-    //     "Yoda"
-    // ];

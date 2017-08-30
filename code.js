aae96cb19e524713f03b91785547ed7c872ab09f
adamberntsson <adam@flonko.com>
2017-08-30 15:03:21 +0200

    added quiz feature

--- code.js
+++ code.js
@@ -1,49 +1,72 @@
 var app = angular.module("quiz",[]);
 
 app.controller("quizController", function($scope){
-    $scope.option = [
-        "4-Lom",
-        "Adiral Motti",
-        "Anakin Skywalker",
-        "Bail Organa",
-        "BG-81",
-        "Bossk",
-        "Captain Needa",
-        "Ello Asty",
-        "EV-9D9",
-        "Galen Erso",
-        "General Hux",
-        "General Tarpas",
-        "Greedo",
-        "Han Solo",
-        "IG-88",
-        "Jabba The Hutt",
-        "Jar Jar Quiz",
-        "K-2SO",
-        "Kit Fisto",
-        "Kylo Ren",
-        "Lando Calrissian",
-        "Lobot",
-        "Luke Skywalker",
-        "Mace Windu",
-        "Max Rebo",
-        "Nien Numb",
-        "Obi-Wan Kenobi",
-        "Oola",
-        "Paploo",
-        "Poe Dameron",
-        "Ponda Baba",
-        "QT-KT",
-        "R5-D4",
-        "Rancor-Keeper",
-        "Salacious B Crumb",
-        "Sebulba",
-        "Silman",
-        "TC-14",
-        "Unkar Plutt",
-        "Watto",
-        "Wedge Antilles",
-        "Yoda"
-    ];
-
+    $scope.questions = [
+      {       image : "images/r2-d2.jpeg",
+              choices : [
+                      "C-3PO",
+                      "QT-KT",
+                      "R5-D404",
+                      "R2-D2"
+                        ],
+              correct : "R2-D2"
+        },
+        {       image : "images/admiral-ackbar.jpeg",
+                choices : [
+                        "Adiral Motti",
+                        "Admiral Ackbar",
+                        "General Hux",
+                        "Galen Erso"
+                          ],
+                correct : "Admiral Ackbar"
+          }
+        ]
 });
+
+
+
+    // ]
+    // $scope.option = [
+    //     "4-Lom",
+    //     "Adiral Motti",
+    //     "Anakin Skywalker",
+    //     "Bail Organa",
+    //     "BG-81",
+    //     "Bossk",
+    //     "Captain Needa",
+    //     "Ello Asty",
+    //     "EV-9D9",
+    //     "Galen Erso",
+    //     "General Hux",
+    //     "General Tarpas",
+    //     "Greedo",
+    //     "Han Solo",
+    //     "IG-88",
+    //     "Jabba The Hutt",
+    //     "Jar Jar Quiz",
+    //     "K-2SO",
+    //     "Kit Fisto",
+    //     "Kylo Ren",
+    //     "Lando Calrissian",
+    //     "Lobot",
+    //     "Luke Skywalker",
+    //     "Mace Windu",
+    //     "Max Rebo",
+    //     "Nien Numb",
+    //     "Obi-Wan Kenobi",
+    //     "Oola",
+    //     "Paploo",
+    //     "Poe Dameron",
+    //     "Ponda Baba",
+    //     "QT-KT",
+    //     "R5-D4",
+    //     "Rancor-Keeper",
+    //     "Salacious B Crumb",
+    //     "Sebulba",
+    //     "Silman",
+    //     "TC-14",
+    //     "Unkar Plutt",
+    //     "Watto",
+    //     "Wedge Antilles",
+    //     "Yoda"
+    // ];

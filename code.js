e854047bb6ea7171ac56e6781df1ae782c797a07
Joakim <joakimj15@gmail.com>
2017-08-30 14:19:50 +0200

    added options to $scope.option

--- code.js
+++ code.js
@@ -1,6 +1,49 @@
 var app = angular.module("quiz",[]);
 
-app.controller("quizController",function($scope){
-  $scope.option = ["", ];
+app.controller("quizController", function($scope){
+    $scope.option = [
+        "4-Lom",
+        "Adiral Motti",
+        "Anakin Skywalker",
+        "Bail Organa",
+        "BG-81",
+        "Bossk",
+        "Captain Needa",
+        "Ello Asty",
+        "EV-9D9",
+        "Galen Erso",
+        "General Hux",
+        "General Tarpas",
+        "Greedo",
+        "Han Solo",
+        "IG-88",
+        "Jabba The Hutt",
+        "Jar Jar Quiz",
+        "K-2SO",
+        "Kit Fisto",
+        "Kylo Ren",
+        "Lando Calrissian",
+        "Lobot",
+        "Luke Skywalker",
+        "Mace Windu",
+        "Max Rebo",
+        "Nien Numb",
+        "Obi-Wan Kenobi",
+        "Oola",
+        "Paploo",
+        "Poe Dameron",
+        "Ponda Baba",
+        "QT-KT",
+        "R5-D4",
+        "Rancor-Keeper",
+        "Salacious B Crumb",
+        "Sebulba",
+        "Silman",
+        "TC-14",
+        "Unkar Plutt",
+        "Watto",
+        "Wedge Antilles",
+        "Yoda"
+    ];
 
-}
+});
